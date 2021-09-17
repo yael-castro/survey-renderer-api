0919178439dc24b31c267ae2053badad32967b0d
Yael Castro <yy.lgnd@gmail.com>
2021-09-17 03:45:25 -0500

    Created interface SurveyRenderer
    SurveyRenderer must be implement "RenderSurvey(model.Survey) (io.Reader,
    error)"
    
    * Created singleton for survey template (variable service.surveyTemplate)
    
    * Function "service.ParseSurveyTemplate" to initialize
    "service.surveyTemplate" was created
    
    * Created constructor for survey template
    
    * Created constructor for service.SurveyRenderer
    
    issue #5

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,2 +1,56 @@
 // Package service contains the business logic
 package service
+
+import (
+	"bytes"
+	"io"
+	"io/fs"
+	"surver-renderer-api/internal/model"
+	"sync"
+	"text/template"
+)
+
+// surveyTemplate use to survey an template
+var surveyTemplate *template.Template
+// onceParseTemplate used to initialize the survey template once
+var onceParseTemplate sync.Once
+
+// NewSurveyTemplate returns a survey template (singleton).
+// Initialize singleton with ParseSurveyTemplate
+func NewSurveyTemplate() *template.Template {
+	return surveyTemplate
+}
+
+// ParseSurveyTemplate use to parse and save the survey template to can be use NewSurveyTemplate.
+// Important: must be called before of the function NewSurveyTemplate
+func ParseSurveyTemplate(fileSystem fs.FS, name string) (err error) {
+	onceParseTemplate.Do(func() {
+		surveyTemplate, err = template.ParseFS(fileSystem, name)
+	})
+	return
+}
+
+// SurveyRenderer use to build a survey template
+type SurveyRenderer interface {
+	// RenderSurvey use to render survey and save it in io.Reader
+	RenderSurvey(model.Survey) (io.Reader, error)
+}
+
+// NewSurveyRenderer constructs a new SurveyRenderer
+func NewSurveyRenderer() SurveyRenderer {
+	return &surveyRenderer{
+		Template: NewSurveyTemplate(),
+	}
+}
+
+type surveyRenderer struct {
+	*template.Template
+}
+
+func (sr *surveyRenderer) RenderSurvey(survey model.Survey) (io.Reader, error) {
+	buffer := bytes.NewBuffer([]byte{})
+
+	err := surveyTemplate.Execute(buffer, survey)
+
+	return buffer, err
+}
