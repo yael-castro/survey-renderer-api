912048a5977316dddb16494ba3812c17dd0998dc
Yael Castro <yy.lgnd@gmail.com>
2022-02-08 00:08:07 -0600

    Updated test for health check connections to Mongo
    I updated tests for health check to MongoDB connections:
    
    - Two tests case were added to test a remote and local connections
    
    issue #7

--- internal/repository/connections_test.go
+++ internal/repository/connections_test.go
@@ -1,40 +1,46 @@
 package repository
 
 import (
-	"os"
 	"strconv"
 	"testing"
 )
 
 var (
-	defaultMongoUser     = os.Getenv("MONGO_USER")
-	defaultMongoPassword = os.Getenv("MONGO_PASSWORD")
+	defaultSettings = Configuration{
+		Host:     "test.efago.mongodb.net",
+		Database: "test",
+		User:     "tester",
+		Password: "tester",
+		Secure:   true, // +srv = port 27_018
+	}
+
+	localSettings = Configuration{
+		Host:     "localhost",
+		Port:     27_017, // default port "27_017"
+		Database: "test",
+		User:     "tester",
+		Password: "tester",
+	}
 
 	// ttNewMongoDatabase contains all tests case for NewMongoDatabase
 	ttNewMongoDatabase = []struct {
 		Configuration
 		expectedError error
 	}{
+		// This case tests the remote connection to MongoDB
+		{Configuration: defaultSettings},
 		// This case tests the connection to MongoDB using a local configuration
-		{
-			Configuration: Configuration{
-				Host:     "localhost",
-				Port:     27_017,
-				Database: "test",
-				User:     defaultMongoUser,
-				Password: defaultMongoPassword,
-			},
-		},
+		{Configuration: localSettings},
 	}
 )
 
-// TestMongoDatabase health check for MongoDB connection
-func TestMongoDatabase(t *testing.T) {
+// TestNewMongoDatabase health check for MongoDB connections
+func TestNewMongoDatabase(t *testing.T) {
 	for i, v := range ttNewMongoDatabase {
 		t.Run(strconv.Itoa(i+1), func(t *testing.T) {
 			_, err := NewMongoDatabase(v.Configuration)
 			if err != nil {
-				t.Fatal(err)
+				t.Fatalf(`%v: %+v`, err, v.Configuration)
 			}
 
 			t.Logf(`%+v`, v.Configuration)
